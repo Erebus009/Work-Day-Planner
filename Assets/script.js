e0057588e8bf1743f70e95da9c7d02dc9c458bde
Travis Puryear <travispuryear678@gmail.com>
2021-09-29 15:59:10 -0400

    first init

---
+++ Assets/script.js
@@ -0,0 +1,22 @@
+let saveBtn = document.querySelector('.save')
+
+
+$(document).ready(function(){
+    
+    
+
+
+
+
+    window.setInterval(function () {
+       $('#time').html(moment().format('ddd MM/DD/y H:mm:ss'))
+   }, 1000);
+   
+
+
+});
+
+
+
+
+
